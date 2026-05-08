c6f1f0c66bc59affd6017461d9bc55881c8a594c
lokeshreddy306 <lokeshasi306@gmail.com>
2026-05-08 17:33:37 +0000

    adding templates

--- terraform-gcp/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "portfolio-ai-tf-state"
-    prefix = "terraform/state"
-  }
-}
--- terraform-gcp/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "google" {
-  project = "portfolio-ai-project-495714"
-  region  = "asia-south1"
-  zone    = "asia-south1-a"
-}
